97903b846867bf4558225672a85e7c57c81e6266
hiddenpoool <kuznetzov.nick@yandex.ru>
2018-04-27 17:34:33 +0400

    init

--- json_to_html/convector2.py
+++ json_to_html/convector2.py
@@ -1,11 +1,5 @@
 import json
-b = {
-    'title': '<h1>%s</h1>',
-    'body': '<p>%s</p>',
-    'h3': '<h3>%s</h3>',
-    'div': '<div>%s</div>'
-
-}
+b = []
 def main():
     path = 'source.json'
     with open(path,'r') as f:
@@ -14,8 +8,7 @@ def main():
     a = str()
     for i in text:
         for  p in i.keys():
-            if p in b.keys():
-                a = a + b[p] % i[p]
+            a = '%s<%s>%s</%s>' %(a,p,i[p],p)
     print(a)
     print('<h3>Title #1</h3><div>Hello, World 1!</div>' == a)
 
